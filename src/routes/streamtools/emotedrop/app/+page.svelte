5046edebc4749ed76ff3be87017a0a024bfe14e5
JCGithu <jack.c.gracie@gmail.com>
2023-02-14 00:20:44 +0000

    moving app to it's own page

---
+++ src/routes/streamtools/emotedrop/app/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  // Add app and name
+  import EmoteDrop from "../EmoteDrop.svelte";
+  let appName = "emotedrop";
+  import { defaultParams } from "../paramsEmoteDrop";
+  //
+
+  import "../../../../css/default.scss";
+  import { onMount } from "svelte";
+  import { runningApp } from "../../params";
+
+  let params = defaultParams;
+  let runApp = true;
+
+  onMount(async () => {
+    let urlData = new URLSearchParams(window.location.search);
+    runApp = urlData.has("data");
+    params = await runningApp(urlData, appName);
+
+    // APP SPECIFIC
+  });
+</script>
+
+{#key params.channel}
+  <EmoteDrop {params} {runApp} />
+{/key}
