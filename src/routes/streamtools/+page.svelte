bcdf9d538f8f92fa0b1189a4b73689257a7a4177
JCGithu <jack.c.gracie@gmail.com>
2023-02-16 17:04:08 +0000

    adding streamtools redirect

---
+++ src/routes/streamtools/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  onMount(() => {
+    let baseURL = window.location.href;
+    window.location.replace(baseURL.split("/streamtools")[0]);
+  });
+</script>
