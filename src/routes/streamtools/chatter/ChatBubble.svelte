987841c53e82fc3c37df76f0f0b5e3d5917c6b5c
JCGithu <jack.c.gracie@gmail.com>
2023-02-16 17:03:11 +0000

    patching animations

--- src/routes/streamtools/chatter/ChatBubble.svelte
+++ src/routes/streamtools/chatter/ChatBubble.svelte
@@ -12,8 +12,6 @@
   let first = false;
   let bits = false;
 
-  let fullShadow = `1px 1px var(--shadowCol), 2px 2px var(--shadowCol), 3px 3px var(--shadowCol), 4px 4px var(--shadowCol), 5px 5px var(--shadowCol), 6px 6px var(--shadowCol), 7px 7px var(--shadowCol)`;
-
   //PARSING
   function runBadge() {
     if (!message.tags.badges) return;
@@ -21,9 +19,9 @@
       if (badgeData[k]) {
         let variations = badgeData[k].versions;
         if (Object.keys(variations).length === 1) {
-          badges.push(variations[Object.keys(variations)[0]]["image_url_4x"]);
+          badges.push(variations[Object.keys(variations)[0]]["image_url_2x"]);
         } else if (variations[message.tags.badges[k]]) {
-          badges.push(variations[message.tags.badges[k]]["image_url_4x"]);
+          badges.push(variations[message.tags.badges[k]]["image_url_2x"]);
         }
       }
     });
@@ -31,96 +29,89 @@
   if (params.badges) runBadge();
 
   //ANIMATIONS
-  function PopIn(el: HTMLElement, size: DOMRect) {
-    el.style.margin = `0px var(--marginX) 0px var(--marginX)`;
-    el.style.padding = `0px var(--paddingX) 0px var(--paddingX)`;
-    el.style.visibility = "hidden";
-    el.style.maxHeight = "0";
-    setTimeout(() => {
-      el.style.transition = "all var(--animTime) var(--animEase)";
-      el.style.visibility = "visible";
-      el.style.padding = `var(--paddingY) var(--paddingX) var(--paddingY) var(--paddingX)`;
-      el.style.margin = `var(--marginY) var(--marginX) var(--marginY) var(--marginX)`;
-      el.style.opacity = "1";
-      el.style.maxHeight = `${size.height + 30}px`;
+  function PopIn(size: DOMRect) {
+    bubble.style.margin = `0px var(--marginX) 0px var(--marginX)`;
+    bubble.style.padding = `0px var(--paddingX) 0px var(--paddingX)`;
+    bubble.style.visibility = "hidden";
+    bubble.style.maxHeight = "0";
+    requestAnimationFrame(() => {
+      bubble.style.transition = "all var(--animTime) var(--animEase)";
+      bubble.style.visibility = "visible";
+      bubble.style.padding = `var(--paddingY) var(--paddingX) var(--paddingY) var(--paddingX)`;
+      bubble.style.margin = `var(--marginY) var(--marginX) var(--marginY) var(--marginX)`;
+      bubble.style.opacity = "1";
+      bubble.style.maxHeight = `${size.height + 30}px`;
       if (params.highlight) {
-        el.style.transform = `translateX(-8px) translateY(-8px)`;
-        el.style.boxShadow = fullShadow;
+        bubble.style.transform = `translateX(-8px) translateY(-8px)`;
       }
-    }, 100);
+    });
   }
 
-  function slideInBoth(el: HTMLElement) {
-    el.style.opacity = "1";
-    setTimeout(() => {
-      el.style.transition = "all var(--animTime) var(--animEase)";
-      if (params.highlight) {
-        el.style.transform = `translateX(-8px) translateY(-8px)`;
-      } else {
-        el.style.transform = `translateX(-8px)`;
-      }
-    }, 100);
+  function slideInBoth() {
+    bubble.style.opacity = "1";
+    requestAnimationFrame(() => {
+      bubble.style.transition = "all var(--animTime) var(--animEase)";
+      bubble.style.transform = `${params.highlight ? "translateX(-8px) translateY(-8px)" : ""}`;
+    });
   }
 
-  function SlideInLeft(el: HTMLElement, size: DOMRect) {
+  function SlideInLeft(size: DOMRect) {
     if (params.align === "center") size.width = size.width * 2.5;
     let negative = "* -1";
     if (params.align === "flex-end") negative = " * 1.2";
-    el.style.transform = `translateX(calc( (${size.width}px + var(--paddingX) + var(--marginX)) ${negative}))`;
+    bubble.style.transform = `translateX(calc( (${size.width}px + var(--paddingX) + var(--marginX)) ${negative}))`;
     if (params.highlight) {
-      el.style.transform = `translateX(calc( (${size.width}px + var(--paddingX) + var(--marginX)) ${negative})) translateY(-8px)`;
-      el.style.boxShadow = fullShadow;
+      bubble.style.transform = `translateX(calc( (${size.width}px + var(--paddingX) + var(--marginX)) ${negative})) translateY(-8px)`;
     }
-    slideInBoth(el);
+    slideInBoth();
   }
 
-  function SlideInRight(el: HTMLElement, size: DOMRect) {
-    el.style.transform = `translateX(calc( (${size.width}px + var(--paddingX) + var(--marginX))))`;
-    if (params.highlight) {
-      el.style.transform = `translateX(calc( (${size.width}px + var(--paddingX) + var(--marginX)))) translateY(-8px)`;
-      el.style.boxShadow = fullShadow;
-    }
-    slideInBoth(el);
+  function SlideInRight(size: DOMRect) {
+    bubble.style.transform = `translateX(calc( (${size.width}px + var(--paddingX) + var(--marginX)))) ${params.highlight ? "translateY(-8px)" : ""}`;
+    slideInBoth();
   }
 
-  function FadeIn(el: HTMLElement) {
-    el.style.opacity = "0";
-    if (params.highlight) {
-      el.style.transform = `translateX(-8px) translateY(-8px)`;
-      el.style.boxShadow = fullShadow;
-    }
-    setTimeout(() => {
-      el.style.transition = "all var(--animTime) var(--animEase)";
-      el.style.opacity = "1";
-    }, 100);
+  function FadeIn() {
+    bubble.style.opacity = "0";
+    if (params.highlight) bubble.style.transform = `translateX(-8px) translateY(-8px)`;
+    requestAnimationFrame(() => {
+      bubble.style.transition = "all var(--animTime) var(--animEase)";
+      bubble.style.opacity = "1";
+    });
   }
 
-  function Grow(el: HTMLElement, size: DOMRect) {
-    el.style.margin = `0px var(--marginX) 0px var(--marginX)`;
-    el.style.padding = `0px var(--paddingX) 0px var(--paddingX)`;
-    el.style.transform = `scale(0)`;
-    el.style.maxHeight = "0";
+  function Grow(size: DOMRect) {
+    bubble.style.margin = `0px var(--marginX) 0px var(--marginX)`;
+    bubble.style.padding = `0px var(--paddingX) 0px var(--paddingX)`;
+    bubble.style.transform = `scale(0)`;
+    bubble.style.maxHeight = "0";
     if (params.highlight) {
-      el.style.boxShadow = fullShadow;
-      el.style.transform = `translateX(-8px) translateY(-8px) scale(0)`;
+      bubble.style.transform = `translateX(-8px) translateY(-8px) scale(0)`;
     }
-    setTimeout(() => {
-      el.style.transition = "all var(--animTime) var(--animEase)";
-      el.style.padding = `var(--paddingY) var(--paddingX) var(--paddingY) var(--paddingX)`;
-      el.style.margin = `var(--marginY) var(--marginX) var(--marginY) var(--marginX)`;
-      el.style.maxHeight = `${size.height + 30}px`;
-      el.style.transform = `scale(1)`;
-      el.style.opacity = "1";
+    requestAnimationFrame(() => {
+      bubble.style.transition = "all var(--animTime) var(--animEase)";
+      bubble.style.padding = `var(--paddingY) var(--paddingX) var(--paddingY) var(--paddingX)`;
+      bubble.style.margin = `var(--marginY) var(--marginX) var(--marginY) var(--marginX)`;
+      bubble.style.maxHeight = `${size.height + 30}px`;
+      bubble.style.transform = `scale(1)`;
+      bubble.style.opacity = "1";
       if (params.highlight) {
-        el.style.transform = `translateX(-8px) translateY(-8px) scale(1)`;
+        bubble.style.transform = `translateX(-8px) translateY(-8px) scale(1)`;
       }
-    }, 100);
+    });
+  }
+
+  function offScreen() {
+    bubble.style.opacity = "1";
+    bubble.style.transform = `translateX(0px) translateY(0px)`;
+    if (params.highlight) bubble.style.transform = `translateX(-8px) translateY(-8px)`;
   }
 
   //MOUNTED
   onMount(async () => {
     console.log(message);
     let size = bubble.getBoundingClientRect();
+    if (params.highlight) bubble.style.boxShadow = `1px 1px var(--shadowCol), 2px 2px var(--shadowCol), 3px 3px var(--shadowCol), 4px 4px var(--shadowCol), 5px 5px var(--shadowCol), 6px 6px var(--shadowCol), 7px 7px var(--shadowCol)`;
     if (!params.animEase) params.animEase = "ease-in-out";
     params.animation = params.animation.replace(" ", "");
     if (params.animation === "SlideIn") params.animation = "SlideInLeft";
@@ -133,13 +124,17 @@
     bubble.style.setProperty("--userCol", params.bubbleCustom ? userCol : params.chatcolourCalc);
     bubble.style.setProperty("--shadowCol", params.togglecol ? userColAlpha : params.highcolour);
 
-    if (params.animation === "PopIn") PopIn(bubble, size);
-    if (params.animation === "SlideInLeft") SlideInLeft(bubble, size);
-    if (params.animation === "SlideInRight") SlideInRight(bubble, size);
-    if (params.animation === "FadeIn") FadeIn(bubble);
-    if (params.animation === "Grow") Grow(bubble, size);
+    if (!document.hidden) {
+      if (params.animation === "PopIn") PopIn(size);
+      if (params.animation === "SlideInLeft") SlideInLeft(size);
+      if (params.animation === "SlideInRight") SlideInRight(size);
+      if (params.animation === "FadeIn") FadeIn();
+      if (params.animation === "Grow") Grow(size);
+    } else {
+      offScreen();
+    }
 
-    if (message.tags.badges.vip) vip = true;
+    if (message.tags.badges["vip"]) vip = true;
     if (message.tags.mod) mod = true;
     if (message.tags["first-msg"]) first = true;
     if (message.tags.bits) bits = true;
@@ -148,7 +143,7 @@
 
     if (params.emoteOnly) {
       message.message.forEach((el) => {
-        if (typeof el === "string") bigEmote = false;
+        if (!el.code) bigEmote = false;
       });
     } else {
       bigEmote = false;
@@ -164,13 +159,14 @@
           <img src={badge} alt="badge" class="twitchBadge" />
         {/each}
       {/if}
+      <b class="chatName" style="color: {params.nameCustom ? message.tags.color : 'inherit'}">{message.tags["display-name"]}</b>
       {#if params.pronouns && message.pronoun}
-        <span class="pronoun" style="font-family:{params.proFont}; --proColour:{params.proUseCol ? message.tags.color : params.proColour}; border-width: {params.proOutline ? '2px' : '0px'}; background-color: {params.proBG ? 'var(--proColour)' : ''}; color: {params.proBG ? params.proColour : 'var(--proColour)'}">{message.pronoun}</span>
+        <span class="pronoun" class:proOutline={params.proOutline} class:proBG={params.proBG} style="font-family:{params.proFont}; --proColour:{params.proUseCol ? message.tags.color : params.proColour}; color: {params.proBG ? params.proColour : 'var(--proColour)'}">{message.pronoun}</span>
       {/if}
-      <b class="chatName" style="color: {params.nameCustom ? message.tags.color : 'inherit'}">{message.tags["display-name"]}{": "}</b>
+      {": "}
       {#each message.message as word}
         {#if word.code}
-          <img src={word.code} alt={word.text} class={bigEmote ? "bigEmote" : "emote"} />
+          <img src={word.code} alt={word.text} class="emote" class:bigEmote />
           {#if word.num}
             <i>{" "}{word.num}{" "}</i>
           {/if}
@@ -257,13 +253,22 @@
 .pronoun {
   --proColour: #ffffff;
   border-radius: 0.5rem;
-  padding: 0 0.3rem;
   //font-size: 0.8rem;
-  border-style: solid;
   display: inline !important;
   width: fit-content;
   color: var(--proColour);
+}
+
+.proOutline{
+  padding: 0 0.3rem;
+  border-width: 2px;
+  border-style: solid;
   border-color: var(--proColour);
+}
+
+.proBG{
+  padding: 0 0.3rem;
+  background-color: var(--proColour);
 }
 
 @keyframes fadeIn {
