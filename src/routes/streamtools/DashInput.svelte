48ef71d463b14b0a3d879fce87229c6c9732350c
JCGithu <jack.c.gracie@gmail.com>
2023-02-26 11:30:12 +0000

    selection input now headless UI, new range slider

--- src/routes/streamtools/DashInput.svelte
+++ src/routes/streamtools/DashInput.svelte
@@ -2,17 +2,17 @@
   import { createEventDispatcher, afterUpdate, getContext } from "svelte";
   import { hexToHSL } from "../../js/hexToHSL";
 
+  import { slide } from "svelte/transition";
+
+  import { Listbox, ListboxButton, ListboxOptions, ListboxOption } from "@rgossiaux/svelte-headlessui";
+
   export let name: string | null = null;
   export let subtitle: string | null = null;
   export let id = "";
   export let type = "";
   export let min: string | null = null;
   export let max: string | null = null;
-  interface Option {
-    value: string;
-    name: string;
-  }
-  export let ops: Array<Option> = [];
+  export let ops: standardObject = {};
   export let value: any = null;
   export let faded = false;
   export let params: standardObject;
@@ -22,10 +22,15 @@
   let style: { opacity: number } = { opacity: 1 };
   let invert = false;
   let titleBlock = true;
-  let dashInputValue: any = "";
+  let dashInputValue: any;
+  let optionName: string = "";
 
   let valueUpdate = (e: any) => {
-    value = e.target.value;
+    if (!e.target) {
+      value = e.detail;
+    } else {
+      value = e.target.value;
+    }
     if (type === "checkbox") value = e.target.checked;
     if (type === "color") {
       let hsl = hexToHSL(e.target.value);
@@ -35,13 +40,13 @@
     dispatch("valueChange", { value: value, id: id });
   };
 
-  //if (grouped) style.width = "100%";
   let direction = "column";
   if (type === "select" && !subtitle) direction = "row";
   if (type === "color" && !value) value = "#ffffff";
   if (type === "checkbox") titleBlock = false;
   if (faded) style.opacity = 0.5;
-  //let styleString = style.stringify();
+
+  let rangeWidth = 0;
 
   afterUpdate(() => {
     if (!params) return;
@@ -52,16 +57,21 @@
       invert = false;
       if (hsl.l >= 70) invert = true;
     }
-    if (faded) {
-      style.opacity = 0.5;
-    } else {
-      style.opacity = 1;
+    style.opacity = 1;
+    if (faded) style.opacity = 0.5;
+    if (type === "select") {
+      optionName = Object.keys(ops).find((key) => ops[key] === dashInputValue) || "";
     }
-    //stringStyle = style.stringify();
+    if (type === "range") {
+      rangeWidth = dashInputValue;
+      if (max === 10 || max === "10") rangeWidth = dashInputValue * 10;
+      rangeWidth = rangeWidth * 0.94;
+      console.log(rangeWidth);
+    }
   });
 </script>
 
-<div class:grouped class:faded style:--flex={direction}>
+<div class="inputBlock" class:grouped class:faded style:--flex={direction}>
   {#if titleBlock}
     <h2 class="inputName">{name}</h2>
     {#if subtitle}
@@ -69,14 +79,27 @@
     {/if}
   {/if}
   {#if type === "select"}
-    <select {id} value={dashInputValue ? dashInputValue : ops[0].value} on:input={valueUpdate}>
-      {#each ops as op}
-        <option value={op.value}>{op.name}</option>
-      {/each}
-    </select>
+    <Listbox value={dashInputValue ? dashInputValue : ops[Object.keys(ops)[0]]} on:change={valueUpdate} class="listBox" let:open>
+      <ListboxButton class="listBoxButton {open ? 'boxOpen' : ''}">{dashInputValue ? optionName : Object.keys(ops)[0]}</ListboxButton>
+      {#if open}
+        <div class="optionHolder" transition:slide>
+          <ListboxOptions class="listBoxOptions">
+            {#each Object.keys(ops) as option (ops[option])}
+              <ListboxOption class="listBoxOption" value={ops[option]} disabled={option === optionName} let:selected>
+                <span class:listBoxSelected={selected}>
+                  {option}
+                </span>
+              </ListboxOption>
+            {/each}
+          </ListboxOptions>
+        </div>
+      {/if}
+    </Listbox>
   {:else if type === "color"}
-    <div class="colourBlock"><p id="colourfontcolour" class:invert>{value}</p></div>
-    <input {type} {id} value={dashInputValue || ""} on:input={valueUpdate} />
+    <div class="colourBlock">
+      <p id="colourfontcolour" class:invert>{value}</p>
+      <input {type} {id} value={dashInputValue || ""} on:input={valueUpdate} />
+    </div>
   {:else if type === "checkbox"}
     <!-- svelte-ignore a11y-label-has-associated-control -->
     <label style={subtitle ? "flex-direction: column" : ""} class="checkContainer">
@@ -89,6 +112,9 @@
         <p class="inputSubtitle">{subtitle}</p>
       {/if}
     </label>
+  {:else if type === "range"}
+    <p class="rangeValue" style="--width:{rangeWidth}%">{dashInputValue}</p>
+    <input {type} {id} {min} {max} value={dashInputValue || ""} on:input={valueUpdate} />
   {:else}
     <input {type} {id} {min} {max} value={dashInputValue || ""} on:input={valueUpdate} />
   {/if}
@@ -106,7 +132,7 @@
     display: flex;
     flex-direction: var(--flex);
     .inputSubtitle {
-      color: $black;
+      color: $white;
     }
     input[type="text"],
     input[type="number"] {
@@ -136,6 +162,10 @@
     margin: 0.35em 0;
   }
 
+  .inputBlock {
+    width: 85%;
+  }
+
   div {
     position: relative;
     display: flex;
@@ -143,25 +173,6 @@
     padding: 0.2rem 0rem;
     align-items: baseline;
     width: calc(100% - 3rem);
-    .colourBlock {
-      display: flex;
-      position: absolute;
-      width: 100%;
-      height: calc(100% - 1.3rem);
-      margin-top: 1.65rem;
-      z-index: 2;
-      justify-content: center;
-      align-items: center;
-      pointer-events: none;
-      p {
-        font-style: normal;
-        font-weight: normal;
-        color: white;
-        padding: 0;
-        margin-top: 0rem;
-        text-transform: uppercase;
-      }
-    }
   }
 
   input {
@@ -173,7 +184,8 @@
     background-color: $white !important;
     background: white;
     color: $black;
-    width: 100%;
+    width: 90%;
+    //margin-left: 5%;
     transition: all 1s;
     opacity: 0.7;
     &:focus {
@@ -188,6 +200,27 @@
     }
     box-shadow: inset 0px 0px 0px -60px lighten($pink, 10);
   }
+
+  .colourBlock {
+    display: flex;
+    position: relative;
+    width: 100%;
+    height: calc(100% - 1.3rem);
+    z-index: 2;
+    justify-content: center;
+    align-items: center;
+    pointer-events: none;
+    p {
+      position: absolute;
+      font-style: normal;
+      font-weight: normal;
+      color: white;
+      padding: 0;
+      margin: 0;
+      text-transform: uppercase;
+    }
+  }
+
   ::-webkit-color-swatch {
     border: none;
     border-radius: 10px;
@@ -267,30 +300,6 @@
     }
   }
 
-  select {
-    margin: 0.4rem 1rem;
-    padding: 0.5rem 0.5rem;
-    border-radius: 0.5rem;
-    border: none;
-    background-color: $white;
-    opacity: 0.7;
-    transition: 0.3s all;
-    color: $black;
-    cursor: pointer;
-    overflow-y: scroll;
-    font-family: "Poppins";
-    font-size: 1rem;
-    outline: none !important;
-    &:hover {
-      opacity: 1;
-      background-color: #008080;
-      color: $white;
-    }
-    &:focus {
-      outline: none;
-    }
-  }
-
   //RANGE
   input[type="range"] {
     appearance: none;
@@ -298,8 +307,10 @@
     transition: opacity 0.2s;
     padding: 0.2rem !important;
     margin: 1rem 0rem;
+    margin-top: 3rem;
     opacity: 1;
     cursor: pointer;
+    width: 100%;
     background-color: darken($black, 5) !important;
   }
   ::-webkit-slider-runnable-track {
@@ -311,13 +322,34 @@
     width: 25px;
     height: 25px;
     border-radius: 100%;
+    position: relative;
     background: $white;
     cursor: pointer;
     transition: 0.2s all;
-    margin-bottom: 0;
+    margin: 0 !important;
     &:hover {
       box-shadow: 0px 0.2rem 5px $black;
       margin-bottom: 0.2rem;
+    }
+  }
+  .rangeValue {
+    color: $white;
+    position: absolute;
+    left: var(--width);
+    width: 35px;
+    height: 30px;
+    padding-top: 5px;
+    background-color: rgba(0, 0, 0, 0);
+    border: 2px solid $colloquial;
+    border-radius: 100%;
+    bottom: 1.7rem;
+    text-align: center;
+    user-select: none;
+    font-weight: bold;
+    transition: all 0.4s cubic-bezier(0.53, 1.31, 0.15, 0.99);
+    &:hover {
+      transform: scale(1.1);
+      background-color: $colloquial;
     }
   }
 
@@ -342,6 +374,116 @@
       margin: 0;
       font-weight: normal;
       padding: 0;
+    }
+  }
+
+  //SELECT
+  .buttonArrow {
+    width: 1.4rem;
+    color: $black;
+    position: absolute;
+  }
+  :global(.listBox) {
+    padding: 0.2rem 1rem;
+    border: none;
+    outline: none;
+    font-family: "Poppins";
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    width: clamp(100%, 250px, 100%);
+  }
+  :global(.listBoxButton) {
+    padding: 0.2rem 1rem;
+    position: relative;
+    width: 100%;
+    max-width: 250px;
+    border: none;
+    outline: none;
+    font-family: "Poppins";
+    color: $black;
+    font-weight: 500;
+    cursor: pointer;
+    border-radius: 0.2rem;
+    border-width: 0px;
+    border-color: fade-out($colloquial, 0.8) fade-out($colloquial, 0.9);
+    border-style: solid;
+    background: linear-gradient(0deg, fade-out($white, 0.5), white);
+    background-size: 600% 600%;
+    background-position: 50% 100%;
+    padding: 1rem;
+    margin-left: 1rem;
+    //box-shadow: 0px 0px 5px darken($pink, 10);
+    //margin-left: 16%;
+    //margin-bottom: 0rem;
+    border-radius: 1rem;
+    z-index: 2;
+    transition: all 0.4s ease-in-out !important;
+    &:hover {
+      border-color: white;
+      //box-shadow: 0px 0px 2px darken($pink, 5);
+      background-position: 50% 0%;
+      //background: linear-gradient(0deg, red, pink);
+    }
+    position: relative;
+    overflow: hidden;
+    &::after {
+      content: url("data:image/svg+xml, %3Csvg class='buttonArrow' viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='m11.125 16.313 7.688-7.688 3.594 3.719-11.094 11.063L0 12.094l3.5-3.531z' /%3E%3C/svg%3E%0A");
+      display: block;
+      position: absolute;
+      transform: scale(0.5);
+      top: 0;
+      right: 0;
+      opacity: 0.5;
+      width: 50px;
+      height: 50px;
+    }
+  }
+  :global(.boxOpen) {
+    //border-radius: 0;
+  }
+  :global(.listBoxOptions) {
+    margin: 0;
+    border-radius: 0 0 0.5rem 0.5rem;
+    overflow: hidden;
+    text-align: center;
+    padding: 0;
+    width: 80%;
+    //margin-left: 1rem;
+    //background-color: darken($white, 5);
+    border: $colloquial 2px solid;
+    border-top: 0px;
+  }
+  .optionHolder {
+    padding: 0;
+    position: relative;
+    margin-left: 1rem;
+    width: 100%;
+    max-width: 250px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  :global(.listBoxOption) {
+    list-style-type: none;
+    cursor: pointer;
+    padding: 0.2rem 1rem;
+    transition: 0.4s all ease-in-out;
+    border-color: $black;
+    &:nth-child(1) {
+      border-width: 0px;
+    }
+    color: $white;
+    border-color: fade-out($white, 0.9);
+    border-width: 0.5px 0px 0px 0px;
+    border-style: solid;
+    border-radius: 0;
+    &:hover {
+      border-radius: 1rem;
+      background-color: lighten($black, 3);
+      //color: $black;
+      font-weight: bold;
     }
   }
 </style>
