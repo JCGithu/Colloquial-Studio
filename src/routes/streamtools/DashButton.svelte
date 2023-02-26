b5489ef608de9827b6186ab0139bcaba2ffb1633
JCGithu <jack.c.gracie@gmail.com>
2023-02-26 11:29:23 +0000

    improved button styling

--- src/routes/streamtools/DashButton.svelte
+++ src/routes/streamtools/DashButton.svelte
@@ -5,27 +5,41 @@
 <button on:click>{text}</button>
 
 <style lang="scss">
+  @import "../../css/colours.scss";
   button {
     font-family: "Poppins";
     font-size: large;
     border: none;
     height: max-content;
-    opacity: 0.5;
-    background-color: #008080;
+    font-weight: light;
+    opacity: 1;
     color: white;
     justify-content: center;
     margin: 0.2rem;
     padding-left: 2rem;
     padding-right: 2rem;
+    margin-bottom: 1rem;
+    border-color: $colloquial;
+    border-style: solid;
+    border-width: 0px;
+    border-top-width: 0.5px;
+    //background-color: rgba(0, 0, 0, 0);
+    background: linear-gradient(290deg, rgba(0, 0, 0, 0) 24%, $pink 25%);
+    background-size: 600% 600%;
+    background-position: 100% 100%;
     border-radius: 1rem;
     display: flex;
     width: max-content;
     align-self: center;
     flex-direction: column;
-    transition: 0.35s cubic-bezier(0.71, 0.7, 0.41, 1.14) 0s !important;
+    transition: 1s cubic-bezier(0.26, 0.77, 0.06, 0.9);
     cursor: pointer;
     &:hover {
       opacity: 1;
+      transform: scale(1.1);
+      border-color: $white;
+      background-position: 50% 100%;
+      box-shadow: 0px 0px 10px fade-out($colloquial, 0.5);
     }
   }
 </style>
