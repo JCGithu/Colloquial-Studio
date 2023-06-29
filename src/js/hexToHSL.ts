ca0c7dfedf465830a76716b1db31d770c0c902ce
JCGithu <jack.c.gracie@gmail.com>
2023-06-29 01:20:45 +0100

    converting to TS

--- src/js/hexToHSL.ts
+++ src/js/hexToHSL.ts
@@ -1,26 +1,26 @@
-// @ts-nocheck
-export function hexToHSL(H) {
+export function hexToHSL(H: any) {
+  H = JSON.stringify(H);
   // Convert hex to RGB first
   let r = 0, g = 0, b = 0;
   if (H.length == 4) {
-    r = "0x" + H[1] + H[1];
-    g = "0x" + H[2] + H[2];
-    b = "0x" + H[3] + H[3];
-  } else if (H.length == 7) {
-    r = "0x" + H[1] + H[2];
-    g = "0x" + H[3] + H[4];
-    b = "0x" + H[5] + H[6];
+    r = parseInt("0x" + H[1] + H[1]);
+    g = parseInt("0x" + H[2] + H[2]);
+    b = parseInt("0x" + H[3] + H[3]);
+  } else if (H.length === 7) {
+    r = parseInt("0x" + H[1] + H[2]);
+    g = parseInt("0x" + H[3] + H[4]);
+    b = parseInt("0x" + H[5] + H[6]);
   }
   // Then to HSL
   r /= 255;
   g /= 255;
   b /= 255;
-  let cmin = Math.min(r,g,b),
-      cmax = Math.max(r,g,b),
-      delta = cmax - cmin,
-      h = 0,
-      s = 0,
-      l = 0;
+  let cmin = Math.min(r, g, b)
+  let cmax = Math.max(r, g, b)
+  let delta = cmax - cmin
+  let h = 0;
+  let s = 0;
+  let l = 0;
 
   if (delta == 0)
     h = 0;
@@ -41,5 +41,5 @@ export function hexToHSL(H) {
   s = +(s * 100).toFixed(1);
   l = +(l * 100).toFixed(1);
 
-  return {"h": h, "s":s, "l": l};
+  return { "h": h, "s": s, "l": l };
 }
