18ed5d859d34706871da5fb20cddb92ac72064af
JCGithu <jack.c.gracie@gmail.com>
2024-01-30 10:21:06 +0000

    adding node polyfills

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from "vite-plugin-top-level-await";
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [wasm(), topLevelAwait(), sveltekit()]
+	plugins: [wasm(), topLevelAwait(),nodePolyfills({include: ['util']}), sveltekit()],
+	optimizeDeps: {
+    include: ['pixi.js'],
+  },
 };
 
 export default config;
