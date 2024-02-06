1773cd8d9ea94d532ac3e8737f04526d209b81cf
JCGithu <jack.c.gracie@gmail.com>
2024-02-06 17:51:56 +0000

    changed to maps for optimisation

--- src/routes/streamtools/chatter/messageParser.ts
+++ src/routes/streamtools/chatter/messageParser.ts
@@ -9,8 +9,8 @@ const cheerRex = new RegExp(otherCheers.join('\\d+|') + `\\d+`, 'gi');
 function checkEmotes(emotes: { [key: number]: Array<string> }, splitText: Array<MessageChunk>, text: string) {
   for (let i in emotes) {
     let emoteArea = emotes[i][0]
-    let parsed = emoteArea.replace('"', '').split('-');
-    let emoteString = text.substring(parseInt(parsed[0]), parseInt(parsed[1]) + 1);
+    const [start, end] = emoteArea.replace('"', '').split('-').map(Number);
+    const emoteString = text.substring(start, end + 1);
     for (let s in splitText) {
       if (splitText[s].text === emoteString) {
         splitText[s] = {
@@ -25,62 +25,54 @@ function checkEmotes(emotes: { [key: number]: Array<string> }, splitText: Array<
 }
 
 function checkBits(splitText: Array<MessageChunk>) {
-  splitText.forEach(chunk => {
+  return splitText.map(chunk => {
     let cheerTest = cheerRex.test(chunk.text.toLowerCase());
-    if (!cheerTest) return;
-    let title = chunk.text.match(/[a-zA-z]+/g)![0];
-    title = title.toLowerCase();
+    if (!cheerTest) return chunk;
+    let title = chunk.text.match(/[a-zA-z]+/g)![0].toLowerCase();
     let num = parseInt(chunk.text.match(/\d+/g)![0]);
     cheerTiers.forEach(rank => {
-      if (num < rank) return;
-      chunk = {
-        code: `https://d3aqoihi2n8ty8.cloudfront.net/actions/${title}/dark/animated/${rank}/2.gif`,
-        num: num,
-        text: chunk.text
+      if (num >= rank) {
+        return {
+          code: `https://d3aqoihi2n8ty8.cloudfront.net/actions/${title}/dark/animated/${rank}/2.gif`,
+          num,
+          text: chunk.text
+        }
       }
     })
+    return chunk;
   })
-  return splitText;
 }
 
 function checkBTTV(bttvEmoteCache: Array<bttvEmote>, splitText: Array<MessageChunk>) {
-  for (let i in splitText) {
+  return splitText.map(chunk => {
     bttvEmoteCache.forEach((BTTVE) => {
-      if (splitText[i].text === BTTVE.code) {
-        splitText[i] = {
+      if (chunk.text === BTTVE.code) {
+        return {
           code: `https://cdn.betterttv.net/emote/${BTTVE.id}/3x`,
-          text: splitText[i].text,
+          text: chunk.text,
         };
       }
     });
-  }
-  return splitText;
+    return chunk;
+  });
 }
 
 function checkFFZ(ffzCache: Array<ffzEmote>, splitText: Array<MessageChunk>) {
-  for (let i in splitText) {
+  return splitText.map(chunk => {
     ffzCache.forEach((FFZE) => {
-      if (splitText[i].text === FFZE.name) {
-        splitText[i] = {
+      if (chunk.text === FFZE.name) {
+        return {
           code: FFZE.urls[Object.keys(FFZE.urls).length - 1],
-          text: splitText[i].text,
+          text: chunk.text,
         };
       }
     });
-  }
-  return splitText;
+    return chunk;
+  });
 }
 
-
 export function formatEmotes(text: string, splitText: Array<string>, emotes: ChatUserstate['emotes'], bttvEmoteCache: Array<bttvEmote>, ffzCache: Array<ffzEmote>, bits: number | undefined): Array<MessageChunk> {
-  let textArray: Array<MessageChunk> = [];
-  for (let i in splitText) {
-    textArray[i] = {
-      code: undefined,
-      text: splitText[i],
-      num: undefined
-    }
-  }
+  let textArray: Array<MessageChunk> = splitText.map(text => ({ code: undefined, text, num: undefined }));
   if (emotes) textArray = checkEmotes(emotes, textArray, text);
   if (bits) textArray = checkBits(textArray);
   let params = get(storage);
