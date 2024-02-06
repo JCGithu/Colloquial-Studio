fd40b3f35c98b6ab71bb77620b396c97e532d3cf
JCGithu <jack.c.gracie@gmail.com>
2024-02-06 17:57:11 +0000

    added page redirect to changelog

---
+++ src/routes/changelog/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+  import { onMount } from "svelte";
+  onMount(() => {
+    window.location.replace("https://jackcgracie.notion.site/Change-Log-31f7cf29cd734013ad0a792d41d88eac");
+  });
+</script>
