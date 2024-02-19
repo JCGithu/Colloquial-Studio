b358d4982f5988f815fb882f7cc1c0e7b92df2cf
JCGithu <jack.c.gracie@gmail.com>
2024-02-19 16:44:27 +0000

    adding twordle redirect

---
+++ src/routes/twordle/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { browser } from "$app/environment";
+  import { goto } from "$app/navigation";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    if (browser) goto("/games/twordle");
+  });
+</script>
